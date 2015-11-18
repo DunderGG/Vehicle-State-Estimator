79bbacbf1c4310366f1ad94bf29a1e0c430d8097
Janu Aditya <yanuart.adityan@gmail.com>
2015-11-18 16:25:31 +0100

    matrix model, computePos, vehicle class added

--- App.cpp
+++ App.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+
+#include "Common.h"				// system configuration
+#include "Vehicle.h" 			// vehicle class (the model)
 #include "Model.h"
 #include "Sensor.h"
 
@@ -10,10 +13,21 @@ using namespace std;
 
 int main()
 {
-	Eigen::MatrixXd m(2,2);
-  	m(0,0) = 3;
-  	m(1,0) = 2.5;
-  	m(0,1) = -1;
-  	m(1,1) = m(1,0) + m(0,1);
-  	cout << m << std::endl;
+ //  	Eigen::MatrixXd m(2,2);
+ //  	m(0,0) = 3;
+ //  	m(1,0) = 2.5;
+ //  	m(0,1) = -1;
+ //  	m(1,1) = m(1,0) + m(0,1);
+ //  	cout << m << std::endl;
+
+  	/* construct the Vehicle */
+    Vehicle volvo (initVector(posX, posY), initVector(velX, velY), initVector(accelX, accelY));
+    
+    /* init the vehicle model */
+    volvo.initMat();
+    
+    /* update (10 second)*/
+    volvo.computePos(simDuration);
+
+    return 0;
 }
---
+++ Common.h
@@ -0,0 +1,32 @@
+//
+//  common.h
+//  _cpp
+//
+//  Created by Yanuar Aditya on 11/16/15.
+//  Copyright © 2015 Chalmers. All rights reserved.
+//
+
+#ifndef common_h
+#define common_h
+
+#include "eigen/Eigen/Dense"
+
+using namespace Eigen;
+
+#define posX            0
+#define posY            0
+#define velX            20      // 72 kph
+#define velY            0       // 0 m/s
+#define accelX          5       // 5m/s
+#define accelY          0
+#define timestep        0.001   // 0.001 s or 1000 fps
+#define constVel        false   // true = constant velocity, false = constant accel
+#define matSize         6     	// 2 blocks for each object, there are 3: pos, vel, acc
+#define simDuration     10		// duration of simulation in second
+
+Eigen::Vector2f initVector (float a, float b) {
+    Eigen::Vector2f temp(a, b);
+    return temp;
+}
+
+#endif /* common_h */
