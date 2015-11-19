06b8ef23fb1802c3beba479ac60245c68ad0acf0
Janu Aditya <yanuart.adityan@gmail.com>
2015-11-19 10:58:42 +0100

    fix App.cpp

--- App.cpp
+++ App.cpp
@@ -2,8 +2,8 @@
 
 #include "Common.h"				// system configuration
 #include "Vehicle.h" 			// vehicle class (the model)
-#include "Model.h"
-#include "Sensor.h"
+#include "Model.h" 				// model
+#include "Sensor.h" 			// sensor
 
 // TO COMPILE WITH EIGEN
 //++ -I ~/Dropbox/Projects/C++/Vehicle-State-Estimator/eigen App.cpp Model.cpp Sensor.cpp Gaussian.cpp -o app
@@ -14,12 +14,19 @@ using namespace Eigen;
 
 int main()
 {
- //  	Eigen::MatrixXd m(2,2);
- //  	m(0,0) = 3;
- //  	m(1,0) = 2.5;
- //  	m(0,1) = -1;
- //  	m(1,1) = m(1,0) + m(0,1);
- //  	cout << m << std::endl;
+	//  Eigen::MatrixXd m(2,2);
+	//  m(0,0) = 3;
+	//  m(1,0) = 2.5;
+	//  m(0,1) = -1;
+	//  m(1,1) = m(1,0) + m(0,1);
+	//  cout << m << std::endl;
+
+	//	float T = 2.0;
+	//
+	//  Model model(52.0, 0);
+	//
+	//	Matrix4f m = model.constVeloModel(5.9);
+	//  	cout << m << endl;
 
   	/* construct the Vehicle */
     Vehicle volvo (initVector(posX, posY), initVector(velX, velY), initVector(accelX, accelY));
@@ -27,20 +34,8 @@ int main()
     /* init the vehicle model */
     volvo.initMat();
     
-    /* update (10 second)*/
+    /* update (10 second), we will have 10/timestep samples of position */
     volvo.computePos(simDuration);
-
-//    return 0;
-//
-//    
-//	float T = 2.0;
-//
-//  	Model model(52.0, 0);
-//
-//
-//	Matrix4f m = model.constVeloModel(5.9);
-//  	cout << m << endl;
-
 
   	return 0;
 }
