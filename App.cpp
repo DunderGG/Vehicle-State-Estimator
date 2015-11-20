ac9d754e7859d5dee7aa5b8c1731a3f25f597b59
Klumpen <david.bennehag@gmail.com>
2015-11-20 14:13:04 +0100

    changed to for

--- App.cpp
+++ App.cpp
@@ -32,7 +32,7 @@ int main()
 	cout << m << endl;
 
 	vector<pair<int, int>> track = sensor.getTrack();
-	for each (pair<int,int> point in track)
+	for (pair<int,int> point : track)
 	{
 		gaussian.getNoise(point);
 	}
